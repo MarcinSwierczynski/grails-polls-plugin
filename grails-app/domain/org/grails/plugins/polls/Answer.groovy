11144e97e65cffe8beaba162e978c4444c404c0f
Marek Podsiadły <marekpodsiadly@gmail.com>
2010-04-10 19:56:42 +0200

    constraints for Answer

--- grails-app/domain/org/grails/plugins/polls/Answer.groovy
+++ grails-app/domain/org/grails/plugins/polls/Answer.groovy
@@ -3,7 +3,11 @@ package org.grails.plugins.polls
 class Answer {
 
     static belongsTo = Poll
+
+    String content
+    int votes = 0
     
     static constraints = {
+        content(blank: false)
     }
 }
