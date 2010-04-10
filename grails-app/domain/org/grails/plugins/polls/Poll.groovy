cfe5bcf3f14fedee041534e67850e6dbe4941fd0
Marek Podsiadły <marekpodsiadly@gmail.com>
2010-04-10 19:52:27 +0200

    constraints for Poll

--- grails-app/domain/org/grails/plugins/polls/Poll.groovy
+++ grails-app/domain/org/grails/plugins/polls/Poll.groovy
@@ -4,6 +4,28 @@ class Poll {
 
     static hasMany = [answers: Answer]
 
+    String question
+    Date start
+    Date end
+    Date dateCreated
+    boolean active = true
+
     static constraints = {
+        question(blank: false)
+        start(nullable: false,
+            validator: {val, obj ->
+                if (!obj?.end)
+                    return true
+                if (val?.compareTo(obj.end) > 0) {
+                    //start have to be before end
+                    return false
+                }
+            }
+        )
+        end(nullable: true)
+    }
+
+    static mapping = {
+        question type: 'text'
     }
 }
