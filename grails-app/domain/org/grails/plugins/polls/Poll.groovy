8559627223155a1a8b4558a054abb5eb8d65059e
Marcin Świerczyński <marcin@swierczynski.net>
2010-04-13 17:57:28 +0200

    Date validator changed

--- grails-app/domain/org/grails/plugins/polls/Poll.groovy
+++ grails-app/domain/org/grails/plugins/polls/Poll.groovy
@@ -4,38 +4,36 @@ import org.codehaus.groovy.grails.commons.ConfigurationHolder
 
 class Poll {
 
-    static hasMany = [answers: Answer]
+	static hasMany = [answers: Answer]
 
-    String question
-    Date startDate
-    Date endDate
-    Date dateCreated
-    boolean active = true
+	String question
+	Date startDate
+	Date endDate
+	Date dateCreated
+	boolean active = true
 
-    static constraints = {
-        question(blank: false)
-        startDate(nullable: false,
-            validator: {val, obj ->
-                if (!obj?.endDate)
-                    return true
-                if (val?.compareTo(obj.endDate) > 0) {
-                    //start have to be before end
-                    return false
-                }
-            }
-        )
-        endDate(nullable: true)
-    }
+	static constraints = {
+		question(blank: false)
+		startDate(nullable: false)
+		endDate(nullable: true,
+				  validator: {val, obj ->
+					  if (val?.compareTo(obj.startDate) < 0) {
+						  //start have to be before end
+						  return false
+					  }
+				  }
+		)
+	}
 
-    static mapping = {
-        def config = ConfigurationHolder.config
-        if(config.grails.polls.poll.table) {
-            table config.grails.polls.poll.table.toString()
-        }
-        else {
-            table "polls"
-        }
+	static mapping = {
+		def config = ConfigurationHolder.config
+		if (config.grails.polls.poll.table) {
+			table config.grails.polls.poll.table.toString()
+		}
+		else {
+			table "polls"
+		}
 
-        question type: 'text'
-    }
+		question type: 'text'
+	}
 }
