57c6e926f3ba9ae07632a7a9dd0a28faf16c0a90
Marek Podsiadły <marekpodsiadly@gmail.com>
2010-04-24 22:37:08 +0200

    test for domain class Poll

--- test/integration/org/grails/plugins/polls/PollIntegrationTests.groovy
+++ test/integration/org/grails/plugins/polls/PollIntegrationTests.groovy
@@ -12,11 +12,33 @@ class PollIntegrationTests extends GrailsUnitTestCase {
         super.tearDown()
     }
 
+    void testBlankQuestion() {
+        def poll = new Poll()
+
+        assertFalse poll.validate()
+        assertTrue poll.hasErrors()
+    }
+
     void testDateValidatingWithEndDateNull() {
         def poll = new Poll(question: 'Sample question')
         def df = new SimpleDateFormat("yyyy-MM-dd")
         poll.startDate = df.parse('2010-04-24')
 
         assertTrue 'Should return true', poll.validate()
+        assertFalse 'Has no error', poll.hasErrors()
+    }
+
+    void testEndDateIsNotBeforeStartDate() {
+        def poll = new Poll(question: 'Sample question')
+        def df = new SimpleDateFormat("yyyy-MM-dd")
+        poll.startDate = df.parse('2010-04-24')
+        poll.endDate = df.parse('2010-04-23')
+
+        assertFalse 'Poll is not valid', poll.validate()
+        assertTrue 'Poll has errors', poll.hasErrors()
+
+        def badField = poll.errors.getFieldError('endDate')
+        assertNotNull "I'm expecting to find an error on the endDate field", badField
+
     }
 }
