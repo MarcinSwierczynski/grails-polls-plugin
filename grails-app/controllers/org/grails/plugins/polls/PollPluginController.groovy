8cd15302954dde9542994785c7c8433e04d9ec1f
Marek Podsiadły <marekpodsiadly@gmail.com>
2010-04-24 10:11:27 +0200

    submit and results action

--- grails-app/controllers/org/grails/plugins/polls/PollPluginController.groovy
+++ grails-app/controllers/org/grails/plugins/polls/PollPluginController.groovy
@@ -2,12 +2,33 @@ package org.grails.plugins.polls
 
 class PollPluginController {
 
-    def submit = {
-        def poll = Poll.get(params.id)
+    def pollService
 
-        if (!poll) {
-            //TODO
+    def submit = {
+        def answer = Answer.get(params.Id)
+
+        if (!answer) {
+            render template: 'error'
             return
         }
+
+        def resuslt = pollService.increaseVotes(answer)
+        if (result) {
+            chain action: 'results', params: [id: answer.poll.id]
+        }
+        else {
+            render template: 'error'
+        }
+    }
+
+    def results = {
+        def poll = Poll.get(id)
+
+        if (!poll) {
+            render template: 'error'
+            return
+        }
+
+        render template: 'results', model: [poll: poll]
     }
 }
---
+++ test/unit/org/grails/plugins/polls/PollPluginControllerTests.groovy
@@ -0,0 +1,17 @@
+package org.grails.plugins.polls
+
+import grails.test.*
+
+class PollPluginControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
